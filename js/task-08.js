321fc5dc2479f0b3e3c36ac16105b70361100e45
argojkee <maksymhaidabura@gmail.com>
2023-07-04 17:08:55 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -21,3 +21,26 @@ function onFormSubmit(event) {
     alert("All fields must be filled");
   }
 }
+
+//FormData
+// const formEl = document.querySelector(".login-form");
+// formEl.addEventListener("submit", onFormSubmit);
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//   const newUser = {};
+
+//   const dataForm = new FormData(event.currentTarget);
+
+//   dataForm.forEach((value, name) => {
+//     newUser[name] = value;
+//   });
+
+//   if (newUser.email && newUser.password) {
+//     console.log(newUser);
+//     formEl.reset();
+//   } else {
+//     alert("All fields must be filled");
+//   }
+// }
